67962222ca43c7472f7533881017d8c087e100f2
han19githtbr <milliance23@gmail.com>
2024-03-16 01:25:49 -0300

    update

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  showHomeLabel: boolean = false;
+  showOuvirLabel: boolean = false;
+  showFalarLabel: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  showLabel(label: string) {
+    switch (label) {
+      case 'home':
+        this.showHomeLabel = true;
+        this.showOuvirLabel = false;
+        this.showFalarLabel = false;
+        break;
+      case 'ouvir':
+        this.showHomeLabel = false;
+        this.showOuvirLabel = true;
+        this.showFalarLabel = false;
+        break;
+      case 'falar':
+        this.showHomeLabel = false;
+        this.showOuvirLabel = false;
+        this.showFalarLabel = true;
+        break;
+      default:
+        this.showHomeLabel = false;
+        this.showOuvirLabel = false;
+        this.showFalarLabel = false;
+        break;
+    }
   }
-
 }
+
+
